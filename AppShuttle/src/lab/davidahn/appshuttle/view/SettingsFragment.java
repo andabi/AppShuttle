5780adeee60bb063e3315aa793272acf420849e2
andabi <dabi.ahn@samsung.com>
2013-11-13 14:56:02 +0900

    amend

--- AppShuttle/src/lab/davidahn/appshuttle/view/SettingsFragment.java
+++ AppShuttle/src/lab/davidahn/appshuttle/view/SettingsFragment.java
@@ -1,7 +1,6 @@
 package lab.davidahn.appshuttle.view;
 
 import lab.davidahn.appshuttle.R;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
@@ -19,7 +18,7 @@ public class SettingsFragment extends PreferenceFragment implements OnSharedPref
 	@Override
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
         if (key.equals("noti.view.enabled")) {
-			getActivity().startService(new Intent(getActivity(), UpdateService.class));
+        	NotiBarNotifier.getInstance().notification();
         }
     }
 	
