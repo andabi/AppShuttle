65c627fb1a1685ae54a286e77341e92c603a27fb
Sanghoon Hong <sanghoon.hong@samsung.com>
2014-02-11 17:22:58 +0900

    [m] getContactName refactoring

--- AppShuttle/src/lab/davidahn/appshuttle/utils/Call.java
+++ AppShuttle/src/lab/davidahn/appshuttle/utils/Call.java
@@ -7,28 +7,30 @@ import android.net.Uri;
 import android.provider.ContactsContract.PhoneLookup;
 
 public class Call {
-	/* Lookup Contact name in String
+	/**
+	 * Lookup Contact name in String
 	 * returns 'null' if there is no contact name with the given number
+	 *
+	 * @param phoneNumber
+	 * @return contactName (null if not exists)
 	 */
 	public static String getContactName(String phoneNumber) {
 		ContentResolver cr = AppShuttleApplication.getContext().getContentResolver();
 		Uri uri = Uri.withAppendedPath(PhoneLookup.CONTENT_FILTER_URI, Uri.encode(phoneNumber));
 		Cursor cursor = cr.query(uri, new String[]{PhoneLookup.DISPLAY_NAME}, null, null, null);
 	
-		if (cursor != null) {
-			String contactName = null;
-	
-			if (cursor.moveToFirst()) {
-				contactName = cursor.getString(cursor.getColumnIndex(PhoneLookup.DISPLAY_NAME));
-			}
-	
-			if (!cursor.isClosed()) 
-				cursor.close();
-	
-			return contactName;
-	
+		if (cursor == null)
+			return null;
+
+		String contactName = null;
+
+		if (cursor.moveToFirst()) {
+			contactName = cursor.getString(cursor.getColumnIndex(PhoneLookup.DISPLAY_NAME));
 		}
-	
-		return null;
+
+		if (!cursor.isClosed())
+			cursor.close();
+
+		return contactName;
 	}
 }
